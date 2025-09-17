41a425b42e3432fe78308f3acbd2ec284e5cce0d
vorobev-andrew <vorobev-andrew>
2025-09-17 12:21:26 +0000

    Bronze edits and beginning silver

--- notebooks/bronze/00_bronze_ingest.py
+++
@@ -1,5 +0,0 @@
-# Make sure the data directories exist
-dbutils.fs.mkdirs("dbfs:/Volumes/claims360_dev/bronze/raw/remit_835")
-dbutils.fs.mkdirs("dbfs:/Volumes/claims360_dev/bronze/raw/ca_277")
-dbutils.fs.mkdirs("dbfs:/Volumes/claims360_dev/bronze/raw/ehr")
-
--- setup/00_init_uc.sql
+++ setup/00_init_uc.sql
@@ -1,6 +1,7 @@
 -- Create catalog
 CREATE CATALOG IF NOT EXISTS claims360_dev
 COMMENT 'Development catalog for Claims360 project';
+USE CATALOG claims360_dev;
 
 -- Schemas
 CREATE SCHEMA IF NOT EXISTS claims360_dev.bronze COMMENT 'Raw/landing + minimally parsed';
@@ -8,20 +9,8 @@ CREATE SCHEMA IF NOT EXISTS claims360_dev.silver COMMENT 'Standardized, deduped,
 CREATE SCHEMA IF NOT EXISTS claims360_dev.gold COMMENT 'De-identified business-ready KPIs & aggregates';
 CREATE SCHEMA IF NOT EXISTS claims360_dev.ops COMMENT 'Runtime artifacts - checkpoints, logs, temp';
 
--- External location, edit storage credential to your specifications
-CREATE EXTERNAL LOCATION IF NOT EXISTS claims360_external
-URL 'abfss://external-storage@claims360.dfs.core.windows.net/'
-WITH (STORAGE CREDENTIAL `avo_databricks_mi`);
-
--- Volumes; using external storage. Point to your storage container
-
-CREATE EXTERNAL VOLUME claims360_dev.bronze.raw
-LOCATION 'abfss://external-storage@claims360.dfs.core.windows.net/claims360/bronze/raw/';
-CREATE EXTERNAL VOLUME claims360_dev.silver.curated
-LOCATION 'abfss://external-storage@claims360.dfs.core.windows.net/claims360/silver/curated/';
-CREATE EXTERNAL VOLUME claims360_dev.gold.publish
-LOCATION 'abfss://external-storage@claims360.dfs.core.windows.net/claims360/gold/publish/';
-CREATE EXTERNAL VOLUME claims360_dev.ops.logs
-LOCATION 'abfss://external-storage@claims360.dfs.core.windows.net/claims360/ops/logs/';
-CREATE EXTERNAL VOLUME claims360_dev.ops.checkpoints
-LOCATION 'abfss://external-storage@claims360.dfs.core.windows.net/claims360/ops/checkpoints/';
+-- Volumes
+CREATE VOLUME IF NOT EXISTS claims360_dev.bronze.raw;
+CREATE VOLUME IF NOT EXISTS claims360_dev.bronze.ingestion;
+CREATE VOLUME IF NOT EXISTS claims360_dev.silver.curated;
+CREATE VOLUME IF NOT EXISTS claims360_dev.gold.publish;
